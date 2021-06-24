8499d7cce85592041a55411491c3e21f88f255d2
Kim <kim.it@ingco.com.ph>
2021-06-24 18:23:11 +0800

    Center Screen Login

--- resources/js/components/Auth/Login.vue
+++ resources/js/components/Auth/Login.vue
@@ -1,32 +1,28 @@
 <template>
     <v-app>
-        <v-main class="" style="">
-            <v-container class="d-flex justify-center" style="">
-                <v-card outlined width="450" style="">
+        <v-container class="d-flex justify-center" style="height: 100%;">
+            <v-card outlined width="450" class="my-auto">
+                <v-progress-linear
+                    v-if="loading.state"
+                    indeterminate
+                    color="green"
+                ></v-progress-linear>
+                <v-card-text>
+                    <v-form class="pa-2">
+                        <div class="col-sm-12 col-md-12">
+                            <input v-model="form.username" autocomplete="off" type="text" class="form-control" placeholder="Username">
+                        </div>
+                        <div class="col-sm-12 col-md-12">
+                            <input v-model="form.password" autocomplete="off" type="password" class="form-control" placeholder="Password">
+                        </div>
 
-                    <v-progress-linear
-                        v-if="loading.state"
-                        indeterminate
-                        color="green"
-                    ></v-progress-linear>
-
-                    <v-card-text>
-                        <v-form class="pa-2">
-                            <div class="col-sm-12 col-md-12">
-                                <input v-model="form.username" autocomplete="off" type="text" class="form-control" placeholder="Username">
-                            </div>
-                            <div class="col-sm-12 col-md-12">
-                                <input v-model="form.password" autocomplete="off" type="password" class="form-control" placeholder="Password">
-                            </div>
-
-                            <div class="col-sm-12 col-md-12">
-                                <v-btn outlined text class="d-flex items-center"><span>Login</span></v-btn>
-                            </div>
-                        </v-form>
-                    </v-card-text>
-                </v-card>
-            </v-container>
-        </v-main>
+                        <div class="col-sm-12 col-md-12">
+                            <v-btn outlined text class="d-flex items-center"><span>Login</span></v-btn>
+                        </div>
+                    </v-form>
+                </v-card-text>
+            </v-card>
+        </v-container>
     </v-app>
 </template>
 
